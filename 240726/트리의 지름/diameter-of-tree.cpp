b6926068c1d73378f95b8977c9fe4043d91217e6
필 수 <104068583+P1su@users.noreply.github.com>
2024-07-26 21:52:48 +0900

    Title: 트리의 지름, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240726/트리의 지름/diameter-of-tree.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define MAX 100000
+
+int N, ans = 0, end = 0;
+vector<pair<int, int>> edges[MAX];
+bool visited[MAX];
+
+void DFS(int x, int dist){
+    if(visited[x])return;
+
+    if(ans < dist){
+        ans = dist;
+        end = x;
+    }
+    for(int i = 0 ; i < edges[x].size() ; i++){
+        visited[x] = true;
+        DFS(edges[x][i].first, dist + edges[x][i].second);
+    }
+}
+
+int main() {
+    cin>>N;
+
+    for(int i = 0 ; i < N ; i++){
+        int a, b, c;
+        cin>>a>>b>>c;
+
+        edges[a].push_back({b, c});
+        edges[b].push_back({a, c});
+
+    }
+
+    DFS(1, 0);
+    memset(visited, false, sizeof(visited));
+    DFS(end, 0);
+
+    cout<<ans;
+    return 0;
+}
